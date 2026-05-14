1a43461af517e40bb69e36d98dde134512ccff80
1inamill <dan.hinojosa@gmail.com>
2026-05-13 21:53:42 -0500

    Refactor code structure for improved readability and maintainability

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 use tauri::{
     menu::{Menu, MenuItem},
     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
-    Manager,
+    Emitter, Manager,
 };
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
